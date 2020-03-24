89d4090cde8916bf1f8fed9e86c30cbfccedf952
niclaflamme <nic@laflamme.io>
2020-03-24 10:15:07 -0400

    Initial Commit

---
+++ wiretap-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wiretap-ios
+//
+//  Created by Nic Laflamme on 2020-03-24.
+//  Copyright © 2020 Nic Laflamme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
